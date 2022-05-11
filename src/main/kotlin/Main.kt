47077439c06d2e091006f34702bd6c06b16815c1
Karina Rocha Fukuda <71196795+karinafukuda@users.noreply.github.com>
2022-05-10 22:51:26 -0300

    added new Class and function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,54 @@
 fun main(args: Array<String>) {
+    //println("Program arguments: ${args.joinToString()}")
+    val contaMaria = Conta()
+    contaMaria.titular = "Maria"
+    contaMaria.numero = 1000
+    contaMaria.saldo = 100.0
+
+    println(contaMaria.titular)
+    println(contaMaria.numero)
+    println(contaMaria.saldo)
+
+    val contaJose = Conta()
+    contaJose.titular = "Jose"
+    contaJose.numero = 1001
+    contaJose.saldo = 200.0
+
+    println(contaJose.titular)
+    println(contaJose.numero)
+    println(contaJose.saldo)
+
+    deposita (contaMaria, 50.0)
+    deposita (contaJose, 10.0)
+    println(contaMaria.saldo)
+    println(contaJose.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun deposita ( conta: Conta, valor: Double){
+    conta.saldo += valor
+}
+
+fun testaReferencias(){
+    val novaConta = Conta()
+    novaConta.titular = "Carlos"
+    println(novaConta.titular)
+}
+
+fun testaCondicoes( saldo: Double){
+    when {
+        saldo > 0.0 -> println("Seu saldo esta positivo")
+        saldo == 0.0 -> println("Sua conta esta zerada")
+        else -> println("Seu saldo esta negativo")
+    }
+}
+
+fun testaLacos(){
     for ( i in 1..3) {
         val titular: kotlin.String = "Karina $i"
         val numeroConta: kotlin.Int = 1234 + i
@@ -29,18 +79,5 @@ fun main(args: Array<String>) {
     while ( k < 3){
         println(k)
         k++
-    }
-
-    //testaCondicoes(saldo)
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    //println("Program arguments: ${args.joinToString()}")
-}
-
-fun testaCondicoes( saldo: Double){
-    when {
-        saldo > 0.0 -> println("Seu saldo esta positivo")
-        saldo == 0.0 -> println("Sua conta esta zerada")
-        else -> println("Seu saldo esta negativo")
     }
 }
